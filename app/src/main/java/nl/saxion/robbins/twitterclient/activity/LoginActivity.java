9700e48e2d7b51b41d9ac70a5449739eb53146fe
Ruben <rubendoesburg_96@hotmail.com>
2016-06-17 14:31:47 +0200

    added stuff, again

--- app/src/main/java/nl/saxion/robbins/twitterclient/activity/LoginActivity.java
+++ app/src/main/java/nl/saxion/robbins/twitterclient/activity/LoginActivity.java
@@ -7,6 +7,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.github.scribejava.core.model.OAuth1RequestToken;
+import com.github.scribejava.core.oauth.OAuth10aService;
 
 import nl.saxion.robbins.twitterclient.R;
 import nl.saxion.robbins.twitterclient.model.AuthManager;
@@ -17,31 +18,38 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        wvLogin = (WebView) findViewById(R.id.wv_login);
-        new RequestTokenTask();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        wvLogin = (WebView) findViewById(R.id.wv_login);
+        RequestTokenTask requestTokenTask = new RequestTokenTask(AuthManager.getInstance().getService());
+        requestTokenTask.execute();
     }
 
     private class RequestTokenTask extends AsyncTask<String, Void, String> {
 
+        OAuth10aService service;
+
+        public RequestTokenTask(OAuth10aService service) {
+            this.service = service;
+        }
+
         @Override
         protected String doInBackground(String... params) {
-            final OAuth1RequestToken requestToken = AuthManager.getInstance().getService().getRequestToken();
-            String authUrl = AuthManager.getInstance().getService().getAuthorizationUrl(requestToken);
+            final OAuth1RequestToken requestToken = service.getRequestToken();
+            String authUrl = service.getAuthorizationUrl(requestToken);
             return authUrl;
         }
 
         @Override
-        protected void onPostExecute(String url) {
-            wvLogin.loadUrl(url);
+        protected void onPostExecute(String authUrl) {
+            wvLogin.loadUrl(authUrl);
             wvLogin.setWebViewClient(new WebViewClient() {
 
                 @Override
                 public boolean shouldOverrideUrlLoading(WebView view, String url) {
                     if (url.startsWith("http://www.google.nl")) {
-                        new AccesTokenTask();
+                        new AccesTokenTask().execute();
                     }
                     return false;
                 }
